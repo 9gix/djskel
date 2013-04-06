e8dfecfc9ceabb680dbe0fbbef36b95f0adfafd2
Eugene <yeo.eugene.oey@gmail.com>
2013-04-07 02:39:50 +0800

    Include MEDIA Serving

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.views.generic import TemplateView
 
@@ -12,4 +14,4 @@ home = HomeView.as_view()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', home, name='home'),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
